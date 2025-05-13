cad7c47170dccf4f55f4db32364da911e7d4d5da
Jason Brown <superbunker@gmail.com>
2025-05-13 14:11:48 -0700

    testing image and link

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -14,6 +14,22 @@ export default function AboutPage() {
                 />
         <p>This is the about page</p>
         <p>This will eventually be a link to home. Testing.</p>
+
+                  <a
+                    className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Image
+                      className="dark:invert"
+                      src="/img/logo.svg"
+                      alt="ARAUM logomark"
+                      width={20}
+                      height={20}
+                    />
+                    Hot Dang
+                  </a>
       </div>
     );
   }
