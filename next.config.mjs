a0ee1d80d3b14d66b708feb4e71ea38b36ee88b2
Jason Brown <superbunker@gmail.com>
2025-05-13 14:36:08 -0700

    testing new path

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         unoptimized: true, // Required for static export
     },
     // If your site will be hosted in a subdirectory, like GitHub Pages project sites
-    basePath: process.env.NODE_ENV === 'production' ? '/repository-name' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/araum-site' : '',
 
     trailingSlash: true, // Recommended for static sites
 
